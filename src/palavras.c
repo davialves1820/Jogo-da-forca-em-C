08da25e7b3873796dd66ff561b868a7ae55b74a3
Davi Alves Rodrigues <144604229+davialves1820@users.noreply.github.com>
2024-12-24 18:45:00 -0300

    Update palavras.c

--- src/palavras.c
+++ src/palavras.c
@@ -1,5 +1,6 @@
 #include "palavras.h"
 
+// Função que vai pegar de forma aleatória uma das palavras do arquivo .txt
 void armazem_palavras(char *str) {
     FILE *f = fopen("data/palavras.txt", "r");
 
@@ -9,23 +10,30 @@ void armazem_palavras(char *str) {
     }
 
     int qnt;
-    fscanf(f, "%d", &qnt);
+    fscanf(f, "%d", &qnt); // Ler a primeira linha do arquivo que está a quantidade de palavras presentes
 
+    // Escolhe um número aleatório que será a posição da palavra a ser adivinhada
     srand(time(NULL));
     int numero_linha = rand() % qnt;
 
-    char palavra[20];
-    for (int i = 1; i < numero_linha; i++) {
+    // Ler as linhas do arquivo até chegar na linha sorteada
+    char palavra[21];
+    for (int i = 0; i < numero_linha; i++) {
         fscanf(f, "%s", palavra);
     }
 
+    // Copia a palavra do arquivo na variável str
     strcpy(str, palavra);
+    
     fclose(f);
 }
 
-void gerar_string(int tam, char *string) {
+// Função que gera uma palavra com caracteres aleatórios
+void gerar_string(int tam, char *str) {
+    srand(time(NULL));
+    
     for (int i = 0; i < tam; i++) {
-        string[i] = rand() % 26 + 'a';
+        str[i] = rand() % 26 + 'a'; // Pega o resto da divisão por 26 e soma com o caracter 'a', a soma resultará em um caracter de acordo com a tabela ASCII
     }
-    string[tam] = '\0';
+    str[tam] = '\0';
 }

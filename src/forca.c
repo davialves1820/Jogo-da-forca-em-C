32c6fd700a1b117ee0f3f8002c571f5ef5fa1e72
Davi Alves Rodrigues <144604229+davialves1820@users.noreply.github.com>
2024-12-24 18:54:33 -0300

    Update forca.c

--- src/forca.c
+++ src/forca.c
@@ -3,23 +3,27 @@
 #include "jogo.h"
 
 int main(void) {
-
+    
     int dificuldade, jogar_novamente;
-    char palavra[20];
+    char palavra[21];
 
+    // Looping de uma partida
     do {
+        // Seleciona a dificuldade escolhida pelo jogador
         do {
-            printf("Qual a dificuldade facil(1) dificil(2): ");
+            printf("Qual a dificuldade? \n1-Medio\n2-Dificil\n ");
             scanf("%d", &dificuldade);
         } while (dificuldade > 2 || dificuldade < 1);
 
         getchar();
+        // Seleciona a palavra de acordo com a dificuldade
         if (dificuldade == 1) {
             armazem_palavras(palavra);
         } else {
             gerar_string(10, palavra);
         }
 
+        // Esconde os caracteres da palavra com '_' e armazena em palavra_escondida
         int tam = strlen(palavra);
         char palavra_escondida[tam];
         esconder_palavra(palavra_escondida, tam);
@@ -27,6 +31,7 @@ int main(void) {
         printf("Boa sorte\n");
         
         int tentativas = 0;
+        // Inicia o looping do jogo 
         do {
             desenhar_forca(tentativas);
             printf("\n%s\n", palavra_escondida);
@@ -40,22 +45,22 @@ int main(void) {
             }
 
             montar_foca(palavra, c, palavra_escondida);
-        } while (tentativas < 7 && strcmp(palavra_escondida, palavra) != 0);
+        } while (tentativas < 7 && strcmp(palavra_escondida, palavra) != 0); // Acaba quando acabarem as tentativas ou a palavra for totalmente desvendada
 
+        // Informa que o jogador perdeu
         if (tentativas == 7) {
-
+            
             printf("\nVoce perdeu :(\n");
             printf("A palavra era %s\n", palavra);
-        } else {
-
+        } else { // Informa que ele ganhou
+            
             printf("\nVoce ganhou :)\n");
             printf("A palavra era %s\n", palavra);
         }
+        // Pergunta se o jogador quer jogar novamente
         printf("Voce quer jogar novamente? \n1-sim\n0-nao\n");
         scanf("%d", &jogar_novamente);
     } while (jogar_novamente == 1);
-
-    
 
     return 0;
 }
